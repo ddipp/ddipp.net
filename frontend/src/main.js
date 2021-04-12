54142c88d44e8e7d756f01ef29848a7feee6d67a
Dmitry Podkovyrkin <dmitryrw@gmail.com>
2021-04-12 22:44:27 +0500

    fix: axios.dev.url

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,10 @@
 import { createApp } from 'vue';
-import axios from 'axios';
+// import axios from 'axios';
 import App from './App.vue';
 // import router from './router'
 import store from './store';
 
-axios.defaults.baseURL = 'http://127.0.0.1:5000/';
+// axios.defaults.baseURL = 'http://127.0.0.1:5000/';
 
 // createApp(App).use(store).use(router).mount('#app')
 createApp(App).use(store).mount('#app');
