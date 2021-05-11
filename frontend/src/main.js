c304570c4c58d0fab6a78233cdbc656c1d36f5d9
Dmitry Podkovyrkin <dmitryrw@gmail.com>
2021-05-11 21:45:01 +0500

    коммит для передачи на дугой компьютер

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,8 @@ import store from './store';
 
 if (process.env.NODE_ENV === 'development') {
   axios.defaults.baseURL = 'http://127.0.0.1:5000/api/';
+} else {
+  axios.defaults.baseURL = '/api/';
 }
 
 createApp(App).use(store).use(router).mount('#app');
